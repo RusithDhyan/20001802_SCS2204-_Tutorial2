b08b813496d9e6d36ba3975e6497247f87ebffa4
RusithDhyan <rusithdhyan2000@gmail.com>
2022-07-10 22:55:16 +0530

    Answers for question 1,2

---
+++ Q1&Q2.scala
@@ -0,0 +1,23 @@
+import scala.compiletime.ops.float
+object main extends App{
+
+    //asigning values  into the variables
+    var i,j,k=2;
+    var m,n=3;          
+    var f:Float=12.0f;
+    var g:Float=4.0f;
+    var c='X';
+
+    println("k + 12*m = "+(k + 12*m));
+    println("m/j = "+(m/j));
+    println("n%j = "+(n%j));
+    println("m/j*j = "+(m/j*j));
+    println("f + 10*5 +g = "+(f+10*5+g));
+    //println("++i*n = "+(++i*n));
+
+
+
+
+
+
+}
