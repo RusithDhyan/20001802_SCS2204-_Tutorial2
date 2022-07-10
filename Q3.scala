792c6d27f5cd8f498754b62ad5691f48f479c4e3
RusithDhyan <rusithdhyan2000@gmail.com>
2022-07-11 01:09:53 +0530

    answers for question 3,4

---
+++ Q3.scala
@@ -0,0 +1,24 @@
+import scala.compiletime.ops.float
+object main extends App{
+
+    //Compare the Java and Scala programming languages
+    var a = 2;
+    var b = 3;
+    var c = 4;
+    var d = 5;
+    var k:Float = 4.3f;
+    var g = 4.0;
+
+    //println(--b*a+c*d--);
+    //println(a++);
+    println("(c) " + (-2*(g-k)+c));
+    //println(c=c++);
+    //println(c=++c*a++);
+
+    /*Increment and decrement operations are not familliar in Scala language.
+    Therefore commented lines are not evaluate at this point*/
+
+
+
+
+}
